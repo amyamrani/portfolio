866755ad15fbec8821ced769b44019c0d295a926
Amy Amrani <vesamrani@gmail.com>
2020-05-17 17:27:52 -0400

    added hamburger menu functionality

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
-$('#about-link').click(() => {
+$('#about-link, #sub-menu-about-link').click(() => {
     $('html, body').animate({
         scrollTop: $('header').offset().top
     }, 200);
 });
 
-$('#projects-link').click(() => {
+$('#projects-link, #sub-menu-projects-link').click(() => {
     $('html, body').animate({
         scrollTop: $('#project-section').offset().top
     }, 200);
 });
 
-$('#contact-link').click(() => {
+$('#contact-link, #sub-menu-contact-link').click(() => {
     $('html, body').animate({
         scrollTop: $('#contact-section').offset().top
     }, 200);
 });
+
+$('.hamburger').click(() => {
+    $('.sub-menu').toggleClass('active-sub-menu')
+});
+
